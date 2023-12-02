f7028824688f3ad9b4fd5aba6fc0f982eda9c572
Jan Pawellek <2392542+janpawellek@users.noreply.github.com>
2023-12-03 00:41:25 +1300

    Add option to load Aifiles on Docker run

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,6 +6,13 @@ if [ -n "$OWNAI_DOWNLOAD_REPO" ] && [ -n "$OWNAI_DOWNLOAD_FILE" ]; then
     flask --app backaind download-model --repo $OWNAI_DOWNLOAD_REPO --filename $OWNAI_DOWNLOAD_FILE
 fi
 
+if [ -n "$OWNAI_ADD_AI" ]; then
+    IFS=":" read -ra filepaths <<< "$OWNAI_ADD_AI"
+    for filepath in "${filepaths[@]}"; do
+        flask --app backaind add-ai --aifile "$filepath"
+    done
+fi
+
 if [ -n "$OWNAI_ROOT_USER" ] && [ -n "$OWNAI_ROOT_PASSWORD" ]; then
     flask --app backaind add-user --username $OWNAI_ROOT_USER --password $OWNAI_ROOT_PASSWORD
 fi
